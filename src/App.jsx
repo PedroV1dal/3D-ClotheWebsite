0a33db273c6a2dbcb517fab747f3ef0b9e47af73
PedroV1dal <pedro.vidal2608@gmail.com>
2023-04-10 14:38:50 -0300

    add animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './index.css'
+import Canvas from "./canvas"
+import Customizer from "./pages/Customizer"
+import Home from "./pages/Home"
+
 
 function App() {
 
   return (
-   <div className="head-text">React App</div>
+   <main className="app transition-all ease-in">
+    <Home />
+    <Canvas />
+    <Customizer />
+   </main>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { snapshot, useSnapshot } from 'valtio'
+
+import state from '../store'
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion'
 
 const Home = () => {
+  const snap = snapshot(state);
+
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home' {...slideAnimation('left')}>
+          <motion.header {...slideAnimation('down')}>
+            <img src="./threejs.png" alt="logo" className='w-8 h-8 object-contain'/>
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation} >
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                LET'S <br className='xl:block hidden'/>
+              </h1>
+            </motion.div>
+          </motion.div>
+          <motion.div
+            {...headContentAnimation}
+            className='flex flex-col gap-5'
+          >
+            <p className='max-w-md font-normal text-gray-600 text-base'>
+              Create the most awesome shirt with this amazing 3D customization tool. <strong>Unleash your imagination</strong>
+            </p>
+
+          
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
